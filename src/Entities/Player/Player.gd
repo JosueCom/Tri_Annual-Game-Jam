8dd4e0d67fcd69c2fdbf09a9dfcf33fb23d03085
Josue N Rivera <josue.n.rivera@outlook.com>
2020-09-04 11:47:18 -0400

    Improvement to parallax bg and new character ctrl

--- src/Entities/Player/Player.gd
+++ src/Entities/Player/Player.gd
@@ -4,25 +4,27 @@ Main Player
 
 extends Entity
 var state_file = preload("PlayerStates.gd")
+var direction := Vector2.RIGHT
+export var wall_cast_len = 25
 
 var STATES := {
-	"IDLE": state_file.Idle.new(),
 	"WALKING": state_file.Walking.new(),
 	"JUMPING": state_file.Jumping.new(),
 	"FALLING": state_file.Falling.new(),
 }
 
 func _ready() -> void:
-	state_machine.init("idle", STATES.IDLE.init(self))
-	state_machine.add_state("walk_run", STATES.WALKING.init(self))
+	state_machine.init("walk_run", STATES.WALKING.init(self))
 	state_machine.add_state("jumping", STATES.JUMPING.init(self))
 	state_machine.add_state("falling", STATES.FALLING.init(self))
+	_velocity.x = 1
 
 func _physics_process(delta: float):
 	state_machine._handle_input()
 	state_machine._physics_process_(delta)
 	
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	print(_velocity)
 	
 func jump_high():
 	jump(jumping_force * 1.3)
--- src/Entities/Player/PlayerStates.gd
+++ src/Entities/Player/PlayerStates.gd
@@ -1,7 +1,6 @@
 
 class Walking extends State:
 	var playback
-	var dir := Vector2.ZERO
 	
 	func init(entity):
 		var out = .init(entity)
@@ -12,87 +11,47 @@ class Walking extends State:
 	
 	func _physics_process_(delta: float) -> void:
 		
-		_entity.move(dir, delta, _entity.moving_force.ground)
-		_entity.decelerate(delta, _entity.friction.ground, -dir.x)
+		_entity.move(_entity.direction, delta, _entity.moving_force.ground)
 		
 	func _handle_input() -> void:
-		dir = Vector2.ZERO
 		
 		if not _entity.is_on_floor():
 			_state_machine.travel("falling")
 		elif Input.is_action_just_pressed("jump"):
 			_state_machine.travel("jumping")
-		elif Input.is_action_pressed("move_right"):
-			dir.x += Input.get_action_strength("move_right")
-			_entity.flip_h(false)
-		elif Input.is_action_pressed("move_left"):
-			dir.x -= Input.get_action_strength("move_left")
-			_entity.flip_h(true)
-		elif(abs(_entity._velocity.x) < 2):
-			_state_machine.travel("idle")
-	
+
 	func _exit() -> void:
 		_entity.get_node("WetSpot").emitting = false
 
 class Falling extends State:
-	var dir := Vector2.ZERO
-		
+
 	func _physics_process_(delta: float) -> void:
-		_entity.move(dir, delta, _entity.moving_force.air)
-		_entity.decelerate(delta, _entity.friction.air)
+		_entity.move(_entity.direction, delta, _entity.moving_force.air)
 		
 	func _handle_input() -> void:
-		dir = Vector2.ZERO
-		
 		if(_entity.is_on_floor()):
-			_state_machine.travel("idle")
-		elif Input.is_action_pressed("move_right"):
-			dir += Vector2.RIGHT
-			_entity.flip_h(false)
-		elif Input.is_action_pressed("move_left"):
-			dir += Vector2.LEFT
-			_entity.flip_h(true)
-
-class Idle extends State:
-		
-	func _enter() -> void:
-		_entity.get_node("WetSpot").emitting = true
-		_entity.get_node("WetSpot").one_shot = true
-
-	func _physics_process_(delta: float) -> void:
-		_entity.decelerate(delta, _entity.friction.ground)
-		
-	func _handle_input() -> void:
-		if Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"):
 			_state_machine.travel("walk_run")
-		elif Input.is_action_pressed("jump"):
+		elif Input.is_action_just_pressed("jump") and _entity.is_on_wall():
 			_state_machine.travel("jumping")
-		elif not _entity.is_on_floor():
-			_state_machine.travel("falling")
-	
-	func _exit() -> void:
-		_entity.get_node("WetSpot").emitting = false
-		_entity.get_node("WetSpot").one_shot = false
 
 class Jumping extends State:
 	var dir := Vector2.ZERO
 
+	func init(entity):
+		var out = .init(entity)
+		return out
+
 	func _enter() -> void:
-		_entity.jump()
+		if _entity.is_on_wall() and not _entity.is_on_floor():
+			_entity.flip_h(not _entity.get_node("Sprite").flip_h)
+			_entity.direction *= -1
+		_entity._velocity.y = -700
 		
 	func _physics_process_(delta: float) -> void:
-		_entity.move(dir, delta, _entity.moving_force.air)
-		_entity.decelerate(delta, _entity.friction.air)
+		_entity.move(_entity.direction, delta, _entity.moving_force.air)
 		
 	func _handle_input() -> void:
-		dir = Vector2.ZERO
-		
-		if Input.is_action_pressed("move_right"):
-			dir += Vector2.RIGHT
-			_entity.flip_h(false)
-		elif Input.is_action_pressed("move_left"):
-			dir += Vector2.LEFT
-			_entity.flip_h(true)
-		
 		if(_entity._velocity.y >= 0 and not _entity.is_on_floor()):
 			_state_machine.travel("falling")
+		elif Input.is_action_just_pressed("jump") and _entity.is_on_wall():
+			_state_machine.travel("jumping")
